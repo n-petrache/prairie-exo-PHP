1bd3ee4748d4b2479c6154d9a597f247fc844f52
n-petrache <petrache.noemie@gmail.com>
2017-01-30 14:11:45 +0100

    addition et multiplication de variables

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $num1 = 1;
+      $num2 = 2;
+      echo $num1 + $num2;
+      echo $num1 * $num2;
+    ?>
+  </body>
+</html>
